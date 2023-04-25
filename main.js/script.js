aa4235c46098c6da3a6a648e00909bd75f87b3a5
hicass <cassidyjwalters@gmail.com>
2023-04-25 13:50:18 -0700

    Turn the board array into a class, with new instances of each board pit. Edit the playerSelects function to work with the new board array.

--- main.js/script.js
+++ main.js/script.js
@@ -6,12 +6,20 @@ const PLAYERS = {
     '-1': '/graphics/moon.PNG'
 }
 
+class Pit {
+    constructor(pitIdx, pebbles, pitType) {
+        this.pitIdx = pitIdx;
+        this.pebbles = pebbles;
+        this.pitType = pitType;
+    }
+}
+
 /*----- state variables -----*/
 // 2. Define the variables that are used to track the game
 // 2.1 The board holes 
-let board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let board;
 // 2.4 The players hand
-let playerHand;
+let playerHand; 
 // 2.5 A winner (Player 1, 2, or Tie)
 let winner;
 // 2.6 Who's turn it is
@@ -37,11 +45,26 @@ document.querySelector('button').addEventListener('click', init)
 /*----- functions -----*/
 // 4. Initialize the state variables
 init();
-  // 4.1 Initialize the state variables:
+// 4.1 Initialize the state variables:
 function init() {
     // 4.1.1 The game starts with empty home bases
     // 4.1.2 The game starts with each main hole having 4 pebbles
-    board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4,];
+    board = [
+    new Pit('p0', 0, 'store-1'),
+    new Pit('p1', 4, 'm1'),
+    new Pit('p2', 4, 'm1'),
+    new Pit('p3', 4, 'm1'),
+    new Pit('p4', 4, 'm1'),
+    new Pit('p5', 4, 'm1'),
+    new Pit('p6', 4, 'm1'),
+    new Pit('p7', 0, 'store1'),
+    new Pit('p8', 4, 'm-1'),
+    new Pit('p9', 4, 'm-1'),
+    new Pit('p10', 4, 'm-1'),
+    new Pit('p11', 4, 'm-1'),
+    new Pit('p12', 4, 'm-1'),
+    new Pit('p13', 4, 'm-1'),
+]
     // 4.1.3 The game starts with player 1
     turn = 1;
     // 4.1.4 The winner is null
@@ -60,14 +83,12 @@ function render() {
 
 // 4.2.1 Render the board:
 function renderBoard() {
-    // 4.2.1.1 Loop over the main holes array, and for each iteration:
-    board.forEach((boardVal, idx) => {
-        // 4.2.1.2 Use the number of that iteration to update the hole at
-        // that index, and update its value to match data
-        const pitEl = document.getElementById(`p${idx}`)
-        // 4.2.1.3 Use the value to display according amount of pebbles
-        pitEl.innerText = boardVal;
-        // 4.2.1.2 Use the home-base value to update its visible pebbles ?????
+    // 4.2.1.1 Loop over the board array, and for each iteration:
+    board.forEach((pit, idx) => {
+        // 4.2.1.2 Get the HTML div with the corresponding ID to the current Pit object
+        const pitEl = document.getElementById(pit.pitIdx);
+        // 4.2.1.3 Use the pebbles property to display according amount of pebbles
+        pitEl.innerText = pit.pebbles
     });
 }
 
@@ -75,7 +96,6 @@ function renderHand() {
     // Updating the hand element text to match data
     handEl.innerText = hand;
 }
-
 
 // 4.2.2. Render the message:
 function renderMessage() {
@@ -95,55 +115,57 @@ function renderMessage() {
 }
 
 // 5. Handle a player clicking a hole
-function playerSelects(pitChoice) {
-    const pitIdx = boardEls.indexOf(pitChoice.target); // Saving the index of the pit that was chosen
-    const pitElement = pitChoice.target
-    const pitValue = board[pitIdx];
-    if (pitIdx === -1) return; // Returning if what was clicked isn't a pit
-    if (!pitElement.classList.contains(`m${turn}`)) return; // Return if what was clicked isn't on the players side
-    // 5.1 Players hand gets updated with the amount of pebbles in that hole
-    hand = board[pitIdx]; // Updating the value of hand to the corresponding value in the board pit that was chosen
-    board[pitIdx] = 0; // Updating the value of the board pit that was chosen to 0
-    dropPebbles(hand, pitIdx);
+function playerSelects(pitElClicked) {
+    const pitElId = pitElClicked.target.id; // Finding the id of the Pit ELement that was clicked
+    const pitObject = board.find((pit) => pit.pitIdx === pitElId); // Finding the corresponding pit in the Board array that has the same ID
+    if (pitElId === 'board') return; // Return if it wasn't a Pit Element that was clicked
+    if (!pitObject.pitType.includes(`m${turn}`)) return; // Return if what was clicked isn't on the players side.
+    // 5.1 Players hand gets updated with the amount of pebbles in the Pit
+    hand = pitObject.pebbles;
+    // 5.2 Updating the amount of pebbles to 0
+    pitObject.pebbles = 0;
+    dropPebbles(hand, pitObject);
     render();
 }
 
-function dropPebbles(hand, idx) {
+function dropPebbles(hand, pit) {
+    console.log(pit)
+    checkForWinner();
     // 5.2 Player deposits one of the pebbles in each hole going counter clock- 
     // wise, until the hand is 0
-    for (i = hand; i > 0; i--) {
-        console.log(i)
-        console.log('idx', idx)
-        console.log('hand:', hand)
-        if (idx === 0) {
-            idx = 8
-            // board[idx]++
-            // hand--
-            console.log('idx === 0')
-        }
-        if (idx === 13) {
-            idx = 7
-            board[idx]++
-            idx = 6
-            console.log('idx === 13')
-        }
-        if (idx >= 7) {
-            idx++
-            board[idx]++
-            hand--
-            console.log('idx > 7')
-        }
-        if (idx < 8) {
-            idx--
-            board[idx]++
-            hand--
-            console.log('idx < 8')
-        }
-    }
-    checkForWinner();
 }
-	 // 5.2.1 If it's their opponents hole, it gets skipped
-   // 5.2.2 If it's their own hole, they place a pebble in it
+
+// 5.2.1 If it's their opponents hole, it gets skipped
+// 5.2.2 If it's their own hole, they place a pebble in it
+// for (i = hand; i > 0; i--) {
+//     console.log(i)
+//     console.log('idx', idx)
+//     console.log('hand:', hand)
+//     if (idx === 0) {
+//         idx = 8
+//         // board[idx]++
+//         // hand--
+//         console.log('idx === 0')
+//     }
+//     if (idx === 13) {
+//         idx = 7
+//         board[idx]++
+//         idx = 6
+//         console.log('idx === 13')
+//     }
+//     if (idx >= 7) {
+//         idx++
+//         board[idx]++
+//         hand--
+//         console.log('idx > 7')
+//     }
+//     if (idx < 8) {
+//         idx--
+//         board[idx]++
+//         hand--
+//         console.log('idx < 8')
+//     }
+// }
      // 5.2.2.1 If it's their last pebble being placed into their own hole
      // they play again
    // 5.2.3 If the last piece is dropped in an empty hole on the players side
