8ee14868c6cb027503acb0fd6cb068941fc17498
hicass <cassidyjwalters@gmail.com>
2023-04-26 11:11:31 -0700

    Add Win/Loss logic, a function to count both players stores to compare for winner, and logic for if the top or bottom row are empty.

--- main.js/script.js
+++ main.js/script.js
@@ -1,7 +1,6 @@
 /*----- constants -----*/
 // 1. Define required constants
 // 1.1 The Players and their info
-
 const PLAYERS = {
     '1': '/graphics/sun.PNG',
     '-1': '/graphics/moon.PNG'
@@ -17,7 +16,6 @@ class Pit {
 
 /*----- state variables -----*/
 // 2. Define the variables that are used to track the game
-// let player
 // 2.1 The board holes 
 let board;
 // 2.4 The players hand
@@ -52,21 +50,21 @@ function init() {
     // 4.1.1 The game starts with empty home bases
     // 4.1.2 The game starts with each main hole having 4 pebbles
     board = [
-    new Pit('p0', 0, 'store-1'),
-    new Pit('p1', 4, 'm1'),
-    new Pit('p2', 4, 'm1'),
-    new Pit('p3', 4, 'm1'),
-    new Pit('p4', 4, 'm1'),
-    new Pit('p5', 4, 'm1'),
-    new Pit('p6', 4, 'm1'),
-    new Pit('p7', 0, 'store1'),
-    new Pit('p8', 4, 'm-1'),
-    new Pit('p9', 4, 'm-1'),
-    new Pit('p10', 4, 'm-1'),
-    new Pit('p11', 4, 'm-1'),
-    new Pit('p12', 4, 'm-1'),
-    new Pit('p13', 4, 'm-1'),
-]
+        new Pit('p0', 0, 'store-1'),
+        new Pit('p1', 4, 'm1'),
+        new Pit('p2', 4, 'm1'),
+        new Pit('p3', 4, 'm1'),
+        new Pit('p4', 4, 'm1'),
+        new Pit('p5', 4, 'm1'),
+        new Pit('p6', 4, 'm1'),
+        new Pit('p7', 0, 'store1'),
+        new Pit('p8', 0, 'm-1'),
+        new Pit('p9', 0, 'm-1'),
+        new Pit('p10', 0, 'm-1'),
+        new Pit('p11', 0, 'm-1'),
+        new Pit('p12', 0, 'm-1'),
+        new Pit('p13', 4, 'm-1'),
+    ]
     // 4.1.3 The game starts with player 1
     turn = 1;
     // 4.1.4 The winner is null
@@ -92,6 +90,7 @@ function renderBoard() {
         // 4.2.1.3 Use the pebbles property to display according amount of pebbles
         pitEl.innerText = pit.pebbles
     });
+    btn.style.visibility = winner ? "visible": "hidden"; 
 }
 
 function renderHand() {
@@ -103,7 +102,7 @@ function renderHand() {
 function renderMessage() {
     // 4.2.2.1 If winner is null, render who's turn it is
     if (winner === null) {
-        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn]}' alt='picture' height='50vmin' width='50vmin'/>'s turn...</h1>`
+        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn]}' alt='picture' id='playerIcon'/>'s turn...</h1>`
     }
     // 4.2.2.2 If winner is 'T', render a tie message
     if (winner === 'T') {
@@ -111,7 +110,7 @@ function renderMessage() {
     }
     // 4.2.2.3 If winner is not null, render congrats msg
     if (winner === 1 || winner === -1) {
-        messageEl.innerText = `Congrats! Player ${winner} wins!`
+        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn]}' alt='picture' id='playerIcon'/> wins!!!</h1>`
     }
     // 4.3 Wait for user to click a hole
 }
@@ -167,17 +166,47 @@ function dropPebbles(hand, pit) {
 
 // 6. Checking for a winner:
 function checkForWinner() {
+    const rightStore = board.find((pit) => pit.pitType === 'store-1');
+    const leftStore = board.find((pit) => pit.pitType === 'store1');
+    const allPits = board.filter((pit) => pit.pitType === 'm1' || pit.pitType === 'm-1');
+    function getPitRowCount(rowType) {
+        let total;
+        const row = board.filter((pit) => {
+            if (pit.pitType === rowType) {
+                return pit;
+            }
+        });
+        total = row.reduce((total, currentVal) => total + currentVal.pebbles, 0);
+        return total;
+    }
+    const topTotal = getPitRowCount('m1');
+    const botTotal = getPitRowCount('m-1');
+    // 6.1 Check if six pockets on one side are empty:
+    // 6.1.1 If yes the other players takes whatever pebbles are left on
+    // their side
+    if (topTotal === 0 || botTotal === 0) {
+        leftStore.pebbles += topTotal;
+        rightStore.pebbles += botTotal;
+        allPits.forEach((pit) => pit.pebbles = 0);
+    }
     if (winner === null) {
         turn *= -1;
     }
-    // 6.1 Check if six pockets on one side are empty:
-     // 6.1.1 If yes the other players takes whatever pebbles are left on
-     // their side
-    // 6.2 Check which player has the most pebbles
-     // 6.2.1 Winner gets updated to the player with the most pebbles
-     // 6.2.2 If player have equal amounts winner gets set to tie
-   // 6.3 Render the updated state
+}
+// 6.2 Check which player has the most pebbles
+function countWinner(leftStore, rightStore) {
+    // 6.2.1 Winner gets updated to the player with the most pebbles
+    if (leftStore > rightStore) {
+        winner = 1;
+    }
+    if (leftStore > rightStore) {
+        winner = -1;
+    }
+    // 6.2.2 If player have equal amounts winner gets set to tie
+    if (leftStore === rightStore) {
+        winner = 'T';
+    }
 }
 
 // 7. Handle player clicking replay button:
-  // 7.1 Step 4 and 4.2
+// 7.1 Step 4 and 4.2
