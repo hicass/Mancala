291ad6b5e5992e7be3752fec051ad8cb8f271f51
hicass <cassidyjwalters@gmail.com>
2023-04-26 21:37:22 -0700

    Change the graphics file name to assets, and make assets .png lowercase. Add CSS effects to make clickable areas clearer, and organize CSS. Remove unnecessary comments in script.js, and rename variables for better readability.

--- main.js/script.js
+++ main.js/script.js
@@ -1,141 +1,137 @@
 /*----- constants -----*/
-// 1. Define required constants
-// 1.1 The Players and their info
-const PLAYERS = {
-    '1': '/graphics/sun.PNG',
-    '-1': '/graphics/moon.PNG'
+class Player {
+    constructor(playerIcon, iconAlt){
+        this.playerIcon = playerIcon;
+        this.iconAlt = iconAlt; 
+    }
 }
 
 class Pit {
-    constructor(pitIdx, pebbles, pitType) {
-        this.pitIdx = pitIdx;
-        this.pebbles = pebbles;
-        this.pitType = pitType;
+    constructor(pitId, pebbles, pitSide, pitPair) {
+        this.pitId = pitId; 
+        this.pebbles = pebbles; 
+        this.pitSide = pitSide; 
+        this.pitPair = pitPair;
     }
+}
+
+const PLAYERS = {
+    '1': new Player('/assets/sun.png', 'sun'), 
+    '-1': new Player('/assets/moon.png', 'moon') 
 }
 
 /*----- state variables -----*/
-// 2. Define the variables that are used to track the game
-// 2.1 The board holes 
-let board;
-// 2.4 The players hand
-let playerHand; 
-// 2.5 A winner (Player 1, 2, or Tie)
+let board; 
+let hand;  
 let winner;
-// 2.6 Who's turn it is
 let turn;
 
 /*----- cached elements  -----*/
-// 3. Store page Els that will be used multiple times:
-// 3.1 The 12 main holes on the board
-const boardEls = [...document.querySelectorAll('#board > div')];
-// 3.2 The 'hand' element that shows how many pebbles are in whatever
-// pit the mouse is hovering over
-const handEl = document.querySelector('#hand');
-// 3.3 The element that displays who's turn it is/ win results
-const messageEl = document.querySelector('h1')
-// 3.4 The play again button
-const btn = document.querySelector('button')
-// 3.5 The pebbles ???
+const boardEls = [...document.querySelectorAll('#board > div')]; 
+const handEl = document.querySelector('#hand'); 
+const messageEl = document.querySelector('h1');
+const btn = document.querySelector('button');
 
 /*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', playerSelects)
-document.querySelector('button').addEventListener('click', init)
+document.getElementById('board').addEventListener('click', playerClicks);
+document.getElementById('board').addEventListener('mouseover', handHover);
+document.querySelector('button').addEventListener('click', init);
 
 /*----- functions -----*/
-// 4. Initialize the state variables
-init();
-// 4.1 Initialize the state variables:
+init(); 
+
 function init() {
-    // 4.1.1 The game starts with empty home bases
-    // 4.1.2 The game starts with each main hole having 4 pebbles
+    // Create a board array using the Pit class, with each pit starting with 4 pebbles
     board = [
         new Pit('p0', 0, 'store-1'),
-        new Pit('p1', 4, 'm1'),
-        new Pit('p2', 4, 'm1'),
-        new Pit('p3', 4, 'm1'),
-        new Pit('p4', 4, 'm1'),
-        new Pit('p5', 4, 'm1'),
-        new Pit('p6', 4, 'm1'),
+        new Pit('p1', 4, 'm1', 'a'),
+        new Pit('p2', 4, 'm1', 'b'),
+        new Pit('p3', 4, 'm1', 'c'),
+        new Pit('p4', 4, 'm1', 'd'),
+        new Pit('p5', 4, 'm1', 'e'),
+        new Pit('p6', 4, 'm1', 'f'),
         new Pit('p7', 0, 'store1'),
-        new Pit('p8', 0, 'm-1'),
-        new Pit('p9', 0, 'm-1'),
-        new Pit('p10', 0, 'm-1'),
-        new Pit('p11', 0, 'm-1'),
-        new Pit('p12', 0, 'm-1'),
-        new Pit('p13', 4, 'm-1'),
+        new Pit('p8', 4, 'm-1', 'f'),
+        new Pit('p9', 4, 'm-1', 'e'),
+        new Pit('p10', 4, 'm-1', 'd'),
+        new Pit('p11', 4, 'm-1', 'c'),
+        new Pit('p12', 4, 'm-1', 'b'),
+        new Pit('p13', 4, 'm-1', 'a'),
     ]
-    // 4.1.3 The game starts with player 1
-    turn = 1;
-    // 4.1.4 The winner is null
-    winner = null;
-    // 4.1.5 The players hand is empty
-    hand = 0;
-    // 4.2 Render the state variables to page:
-    render();
+    turn = 1; 
+    winner = null; 
+    hand = 0; 
+    render(); 
 }
+
+/*----- initialized constants -----*/
+
 
 function render() {
-    renderBoard();
-    renderMessage();
-    renderHand();
+    renderBoard(); 
+    renderMessage(); 
 }
 
-// 4.2.1 Render the board:
 function renderBoard() {
-    // 4.2.1.1 Loop over the board array, and for each iteration:
     board.forEach((pit, idx) => {
-        // 4.2.1.2 Get the HTML div with the corresponding ID to the current Pit object
-        const pitEl = document.getElementById(pit.pitIdx);
-        // 4.2.1.3 Use the pebbles property to display according amount of pebbles
+        // Get the document div that has the matching id to the current pit item
+        const pitEl = document.getElementById(pit.pitId);
+        // If the pit is on the players side give it the available class
+        if (pit.pitSide === `m${turn}`) { 
+            pitEl.setAttribute('class', 'available')
+        } 
+        // If the pit is not on the players side, if theres a winner, or if theres
+        // no pebbles in the pit, remove the available class.
+        if (pit.pitSide !== `m${turn}` || winner !== null || pit.pebbles === 0) {
+            pitEl.removeAttribute('class', 'available')
+        }
         pitEl.innerText = pit.pebbles
     });
-    btn.style.visibility = winner ? "visible": "hidden"; 
+    btn.style.visibility = winner ? 'visible': 'hidden'; 
 }
 
-function renderHand() {
-    // Updating the hand element text to match data
-    handEl.innerText = hand;
-}
-
-// 4.2.2. Render the message:
 function renderMessage() {
-    // 4.2.2.1 If winner is null, render who's turn it is
     if (winner === null) {
-        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn]}' alt='picture' id='playerIcon'/>'s turn...</h1>`
+        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn].playerIcon}' alt='${PLAYERS[turn].iconAlt}' id='playerIcon'/>'s turn...</h1>`
     }
-    // 4.2.2.2 If winner is 'T', render a tie message
+    if (winner === 1 || winner === -1) {
+        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn].playerIcon}' alt='${PLAYERS[turn].iconAlt}' id='playerIcon'/> wins!!!</h1>`
+    }
     if (winner === 'T') {
         messageEl.innerText = `It's a tie!`
     }
-    // 4.2.2.3 If winner is not null, render congrats msg
-    if (winner === 1 || winner === -1) {
-        messageEl.innerHTML = `<h1><img src='${PLAYERS[turn]}' alt='picture' id='playerIcon'/> wins!!!</h1>`
-    }
-    // 4.3 Wait for user to click a hole
 }
 
-// 5. Handle a player clicking a hole
-function playerSelects(pitElClicked) {
-    const pitElId = pitElClicked.target.id; // Finding the id of the Pit ELement that was clicked
-    const pitObject = board.find((pit) => pit.pitIdx === pitElId); // Finding the corresponding pit in the Board array that has the same ID
-    if (pitElId === 'board') return; // Return if it wasn't a Pit Element that was clicked
-    if (!pitObject.pitType.includes(`m${turn}`)) return; // Return if what was clicked isn't on the players side.
-    // 5.1 Players hand gets updated with the amount of pebbles in the Pit
-    hand = pitObject.pebbles;
-    // 5.2 Updating the amount of pebbles to 0
-    pitObject.pebbles = 0;
-    dropPebbles(hand, pitObject);
+function playerClicks(pitElClicked) {
+    const pitElId = pitElClicked.target.id; 
+    const pitInArr = board.find((pit) => pit.pitId === pitElId); 
+    if (pitElId === 'board') return; 
+    if (!pitInArr.pitSide.includes(`m${turn}`)) return; // If the pit wasn't on the players side return
+    if (pitInArr.pebbles === 0) return;
+    hand = pitInArr.pebbles; // The pebbles in the pit get picked up by the hand
+    pitInArr.pebbles = 0; // Pit now has no pebbles
+    dropPebbles(hand, pitInArr);
     render();
 }
 
+// Preview how many pebbles are in each pit
+function handHover(pitElOver) {
+    const pitElId = pitElOver.target.id;
+    const pitInArr = board.find((pit) => pit.pitId === pitElId); 
+    if (pitElId === 'board' || winner !== null) {
+        handEl.style.visibility = 'hidden';
+        return;
+    }
+    handEl.style.visibility = 'visible';
+    handEl.innerText = pitInArr.pebbles;
+}
+
 function dropPebbles(hand, pit) {
-    let pitArrIdx = board.indexOf(pit)
-    // 5.2 Player deposits one of the pebbles in each hole going counter clock- 
-    // wise, until the hand is 0
+    let pitArrIdx = board.indexOf(pit);
+    // Deposit one of the pebbles in each pit going counter clockwise until the hand is 0
     for (i = hand; i > 0; i--) {
-        // 5.2.1 If it's their opponents store, it gets skipped
-        if (board[pitArrIdx].pitType === `store${turn * -1}`) {
+        // If it's the opponents hole skip to the next pit
+        if (board[pitArrIdx].pitSide === `store${turn * -1}`) {
             --board[pitArrIdx].pebbles; 
             pitArrIdx++;
             ++hand;
@@ -144,7 +140,7 @@ function dropPebbles(hand, pit) {
             pitArrIdx++;
             ++board[pitArrIdx].pebbles;
             hand--;
-        } else if (pitArrIdx === 13) {
+        } else if (pitArrIdx === 13) { // Loop the index number back to 0 when it hits 13
             pitArrIdx = 0;
             ++board[pitArrIdx].pebbles;
             hand--;
@@ -152,61 +148,60 @@ function dropPebbles(hand, pit) {
             pitArrIdx++;
             ++board[pitArrIdx].pebbles;
         } 
-        // 5.2.2.1 If it's their last pebble being placed into their own hole
-        // they play again
-        if (i === 1 && board[pitArrIdx].pitType === `store${turn}`) {
+        // Player gets another turn if the last pebble is placed in their own store
+        if (i === 1 && board[pitArrIdx].pitSide === `store${turn}`) {
             turn *= -1;
         }
+        // If the last pebble is dropped in an empty pit on the players side
+        // the pebbles on the pairing pit on the opponents side gets added to the players store
+        if (i === 1 && (board[pitArrIdx].pitSide === `m${turn}` && board[pitArrIdx].pebbles === 1)) {
+            const pairLetter = board[pitArrIdx].pitPair;
+            const pair = board.filter((pit) => pit.pitPair === pairLetter);
+            const pairPebbles = pair.reduce((total, currentVal) => total + currentVal.pebbles, 0);
+            const playerStore = board.find((pit) => pit.pitSide === `store${turn}`)
+            if (pairPebbles > 1) {
+                playerStore.pebbles += pairPebbles;
+                pair.forEach((pit) => pit.pebbles = 0);
+            }
+        }
     }
-    // 5.2.3 If the last piece is dropped in an empty hole on the players side
-    // they get to take the pebbles on the opposing side if any
-    // 5.3 Check for a winner
     checkForWinner();
 }
 
-// 6. Checking for a winner:
 function checkForWinner() {
-    const rightStore = board.find((pit) => pit.pitType === 'store-1');
-    const leftStore = board.find((pit) => pit.pitType === 'store1');
-    const allPits = board.filter((pit) => pit.pitType === 'm1' || pit.pitType === 'm-1');
-    function getPitRowCount(rowType) {
+    const rightStore = board.find((pit) => pit.pitSide === 'store-1'); 
+    const leftStore = board.find((pit) => pit.pitSide === 'store1');
+    const allPits = board.filter((pit) => pit.pitSide === 'm1' || pit.pitSide === 'm-1');
+    function getRowSum(rowType) {
         let total;
-        const row = board.filter((pit) => {
-            if (pit.pitType === rowType) {
-                return pit;
-            }
-        });
+        const row = board.filter((pit) => pit.pitSide === rowType);
         total = row.reduce((total, currentVal) => total + currentVal.pebbles, 0);
         return total;
     }
-    const topTotal = getPitRowCount('m1');
-    const botTotal = getPitRowCount('m-1');
-    // 6.1 Check if six pockets on one side are empty:
-    // 6.1.1 If yes the other players takes whatever pebbles are left on
-    // their side
+    const topTotal = getRowSum('m1');
+    const botTotal = getRowSum('m-1');
+    // If six pits on one side are empty the remaining pebbles on the
+    // other side get added to that players store
     if (topTotal === 0 || botTotal === 0) {
         leftStore.pebbles += topTotal;
         rightStore.pebbles += botTotal;
-        allPits.forEach((pit) => pit.pebbles = 0);
+        allPits.forEach((pit) => pit.pebbles = 0)
+        hand = null;
+        countWinner(leftStore.pebbles, rightStore.pebbles);
     }
     if (winner === null) {
         turn *= -1;
     }
 }
-// 6.2 Check which player has the most pebbles
+
 function countWinner(leftStore, rightStore) {
-    // 6.2.1 Winner gets updated to the player with the most pebbles
     if (leftStore > rightStore) {
         winner = 1;
     }
-    if (leftStore > rightStore) {
+    if (leftStore < rightStore) {
         winner = -1;
     }
-    // 6.2.2 If player have equal amounts winner gets set to tie
     if (leftStore === rightStore) {
-        winner = 'T';
+        winner = 'T'
     }
 }
-
-// 7. Handle player clicking replay button:
-// 7.1 Step 4 and 4.2
